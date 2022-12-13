fc9216c7aa1edc595c45660febac5a4d5a559f36
anastasiia3006 <111446004+Hornostai@users.noreply.github.com>
2022-12-13 11:54:26 +0100

    create

---
+++ hw-8.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+NAME=[]
+BIRTHDAY=[]
+
+USERS={
+    "Monday":"Sacha",
+    "Thuesday":"Olia",
+    "Wednesday":"Peter",
+    "Thuesday":"Ivan",
+    "Friday":"Vlad",
+    "Saturday":"Vika" "Masha",
+    "Sunday": "Anna",
+}
+
+def get_birthdays_per_week(USERS):
+    HDAY = datetime.day(BIRTHDAY)
+    for i in USERS:
+        if BIRTHDAY.range(-1,-2):
+            selebrated = 0
+            for elem in USERS:
+                selebrated += 2
+            print(selebrated)
+
+            print(f"We can selebrated, {NAME[-1,-2]} in Monday ")
+        else:
+            print(f"We are,selebreted today {NAME}")
+    
+
+print(get_birthdays_per_week(USERS))
+        
+
+
+    
+
+
+#def main():
+    
+#if __name__ == '__main__':
+#    main()
+        
