1f27478944bdae00b253b11b24a4fa1bfb98dfa4
Hornostai <111446004+Hornostai@users.noreply.github.com>
2022-12-15 14:37:32 +0100

    Update hw-8.py

--- hw-8.py
+++ hw-8.py
@@ -1,41 +1,48 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import random
+from collections import defaultdict
+
+USERS = defaultdict(str)
+
+USERS[26.08] :"Sacha",
+    30.02 :"Olia",
+    05.09 :"Peter",
+    10.11 :"Ivan",
+    23.10 :"Vlad",
 
 NAME=[]
 BIRTHDAY=[]
 
 USERS={
-    "Monday":"Sacha",
-    "Thuesday":"Olia",
-    "Wednesday":"Peter",
-    "Thuesday":"Ivan",
-    "Friday":"Vlad",
-    "Saturday":"Vika" "Masha",
-    "Sunday": "Anna",
+    26.08 :"Sacha",
+    30.02 :"Olia",
+    05.09 :"Peter",
+    10.11 :"Ivan",
+    23.10 :"Vlad",
 }
 
-def get_birthdays_per_week(USERS):
-    HDAY = datetime.day(BIRTHDAY)
-    for i in USERS:
-        if BIRTHDAY.range(-1,-2):
-            selebrated = 0
-            for elem in USERS:
-                selebrated += 2
-            print(selebrated)
+total =0
+for i in BIRTHDAY:
+    total
 
-            print(f"We can selebrated, {NAME[-1,-2]} in Monday ")
-        else:
-            print(f"We are,selebreted today {NAME}")
-    
+def get_birthdays_per_week(n):
+    current_date = datetime.now()
+    oldest_date = current_date-timedelta(days=365*80)#порівнює дату +-.
+    birthdays_list = []
+    for i in range(5):
+        fake_year = random.randrange(oldest_date.year, current_date.year)
+        fake_month = random.randint(1,12)
+        fake_day = random.randint(1,31)
+        try:
+            fake_birthday = datetime(year=fake_year,month=fake_month, day=fake_day)
+        except ValueError:
+            continue
+        if current_date>=fake_birthday:
+            birthdays_list.append(fake_birthday.date())
+    return birthdays_list
 
-print(get_birthdays_per_week(USERS))
-        
-
-
-    
-
-
+print(USERS)
 #def main():
     
-#if __name__ == '__main__':
-#    main()
-        
+if __name__ == '__main__':
+    print(get_birthdays_per_week(USERS))
