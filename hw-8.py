4040876d58c4193e5183173e1c893a3f0ee8f4e7
Hornostai <hornostai.anastasiia3006@gmail.com>
2022-12-17 13:43:16 +0100

    upgreate

--- hw-8.py
+++ hw-8.py
@@ -2,47 +2,45 @@ from datetime import datetime, timedelta
 import random
 from collections import defaultdict
 
-USERS = defaultdict(str)
 
-USERS[26.08] :"Sacha",
-    30.02 :"Olia",
-    05.09 :"Peter",
-    10.11 :"Ivan",
-    23.10 :"Vlad",
+users = defaultdict(str)
 
-NAME=[]
-BIRTHDAY=[]
+users["5.09.1985"] ="Anna"
+users["6.09.2000"] ="Vika"
+users["7.09.1988"] ="Oleg"
+users["8.09.1996"] ="Sasha"
+users["9.09.1980"] ="Tanya"
+users["10.09.1999"] ="Nastya"
+users["11.09.1990"] ="Vlad"
 
-USERS={
-    26.08 :"Sacha",
-    30.02 :"Olia",
-    05.09 :"Peter",
-    10.11 :"Ivan",
-    23.10 :"Vlad",
-}
+#hear i must try range and change time from calender to time
 
-total =0
-for i in BIRTHDAY:
-    total
 
-def get_birthdays_per_week(n):
-    current_date = datetime.now()
-    oldest_date = current_date-timedelta(days=365*80)#порівнює дату +-.
-    birthdays_list = []
-    for i in range(5):
-        fake_year = random.randrange(oldest_date.year, current_date.year)
-        fake_month = random.randint(1,12)
-        fake_day = random.randint(1,31)
-        try:
-            fake_birthday = datetime(year=fake_year,month=fake_month, day=fake_day)
-        except ValueError:
-            continue
-        if current_date>=fake_birthday:
-            birthdays_list.append(fake_birthday.date())
-    return birthdays_list
+work_days = {"Monday":[],"Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [], "Next Monday": []}
 
-print(USERS)
-#def main():
-    
+def main(users):
+    current_date = datetime.now().date()
+    for k,v in users.items():
+        birthday=datetime.strptime(k,"%d.%m.%Y").date()
+        current_birthday = birthday.replace(year=current_date.year)
+        if current_date<= current_birthday<= current_date+timedelta(days=7):
+            print(current_birthday.weekday()) 
+            if current_birthday.weekday() == 0:
+                uppend_users = users.append(current_birthday.weekday(work_days))
+                for key in uppend_users:
+                    key += ["Monday"]
+                    print(key)
+                    if current_birthday == [5] or [6]:
+                        work_days+=["Next Monday"]
+                        if users in work_days:
+                            print (users)
+
+#Там де перевірка current_birthday.weekday() == 0 чи іншій цифрі ,
+# треба робити операції додавання цих користувачів в списки з днями тижня
+
+# це виводиться номер дня тиждня, зробіть перевірку якщо 0 то додавайте в список work_days по ключу Monday
+# якщо суботта та неділя - то в список по ключу Next Monday
+# потім зробіть вивід в консоль якщо в списку є імениники
+
 if __name__ == '__main__':
-    print(get_birthdays_per_week(USERS))
+    main(users)
