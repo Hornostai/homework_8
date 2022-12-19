5780b6bd7a4ca1e6eb88f21c2711a5b284295f97
Hornostai <111446004+Hornostai@users.noreply.github.com>
2022-12-19 17:51:19 +0100

    upgreate 19.12

--- hw8.py
+++ hw8.py
@@ -9,17 +9,16 @@ from collections import defaultdict
 import random
 from collections import defaultdict
 
-users = defaultdict(str)
-# рекомендую створити дні народження які будуть на наступних 7 днях для тестування
-users["18.12.1985"] = "Anna"
-users["17.10.2000"] = "Vika"
-users["7.09.1988"] = "Oleg"
-users["8.09.1996"] = "Sasha"
-users["9.09.1980"] = "Tanya"
-users["10.09.1999"] = "Nastya"
-users["11.09.1990"] = "Vlad"
+users = defaultdict(int)
+users = [{'name': 'Vika', 'birthday': '17.10.2000'},
+        {"name": "Anna","birthday":"18.12.1985"},
+        {"name": "Oleg","birthday":"07.09.1988"},
+        {"name": "Sasha","birthday":"08.09.1980"},
+        {"name": "Tanya","birthday":"03.11.1990"},
+        {"name": "Nastya","birthday":"25.04.1991"},
+        {"name": "Vlad","birthday":"30.03.2001"}]
+        
 
-# hear i must try range and change time from calender to time
 
 
 work_days = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [], "Next Monday": []}
@@ -27,32 +26,35 @@ work_days = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Frid
 
 def main(users):
     current_date = datetime.now().date()
-    for k, v in users.items():
-        birthday = datetime.strptime(k, "%d.%m.%Y").date()
+    for user in users:
+        birthday = datetime.strptime(user["birthday"],"%d.%m.%Y").date()
         current_birthday = birthday.replace(year=current_date.year)
         if current_date <= current_birthday <= current_date + timedelta(days=7):
             print(current_birthday.weekday())
         elif current_birthday.weekday() == 0:
-                work_days['Monday'].append(v)
+                work_days['Monday'].append(user["name"])
         elif current_birthday.weekday() == 1:
-                work_days['Tuesday'].append(v)
+                work_days['Tuesday'].append(user["name"])
         elif current_birthday.weekday() == 2:
-                work_days['Wednesday'].append(v)
+                work_days['Wednesday'].append(user["name"])
         elif current_birthday.weekday() == 3:
-                work_days["Thursday"].append(v)
+                work_days["Thursday"].append(user["name"])
         elif current_birthday.weekday()==4:
-                work_days["Friday"].append(v)
+                work_days["Friday"].append(user["name"])
         else:
-                current_birthday.weekday() in [5,6]
-                work_days["Next Monday"].append(v)
-        print(work_days)
+             current_birthday.weekday() in [5,6]
+             work_days["Next Monday"].append(user["name"])
+    
                 # v - це ім'я нашого імениника, ми звертаємось в словник work_days # та записуємо в список імениника
                 # тут логіку додавання інші ключі словника
-
+    counter =0
     for key, value in work_days.items():  # після того як наповнили словник імениниками, ітеруємося по ньому
         # та якщо є імениники принтуємо на єкран
         if value:
             print(f'{key}: {", ".join(value)}')
+            counter+= 1
+        if counter ==0:
+             print('Not users with birthday in next 7 days!')   
 
 
 # це виводиться номер дня тиждня, зробіть перевірку якщо 0 то додавайте в список work_days по ключу Monday
@@ -60,4 +62,4 @@ def main(users):
 # потім зробіть вивід в консоль якщо в списку є імениники
 
 if __name__ == '__main__':
-    main(users)
+        main(users)
